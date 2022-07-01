5a4af6f38f8e7161ac63ea80a4d9eb7b804f1b0a
ezeyr <ezeyr@LAPTOP-CUFCK8VV>
2022-07-01 21:30:06 +0300

    Add project files.

---
+++ ClassMetotDemo/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class Customer
+    {
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerSurname { get; set; }
+        public string Address { get; set; }
+        public int PhoneNumber { get; set; }
+    }
+}
